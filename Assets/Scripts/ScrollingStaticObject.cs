2b89c67372993fbb3a769eebadd9f7e2ed12ab7c
Teemu <teemu@rautasydan.org>
2018-12-01 13:55:49 +0200

    Updated Aurora, implemented sky scrolling

---
+++ Assets/Scripts/ScrollingStaticObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingStaticObject : MonoBehaviour {
+    Renderer _r;
+    public Vector2 ScrollSpeed = Vector2.right * 0.1f;
+
+    private void Awake()
+    {
+        _r = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        _r.material.mainTextureOffset += ScrollSpeed / 1000.0f;
+	}
+}
